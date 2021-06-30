006ada4254549e0c1bbf70bf62d995fac6e66ad5
bitfriend <bitfriend@hotmail.com>
2021-06-30 02:09:01 +0200

    Improve connect button ar top sidebar

--- src/widgets/Menu/components/UserBlock.tsx
+++ src/widgets/Menu/components/UserBlock.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components';
 import Button from "../../../components/Button/Button";
 import { useWalletModal } from "../../WalletModal";
 import { Login } from "../../WalletModal/types";
@@ -9,13 +10,32 @@ interface Props {
   logout: () => void;
 }
 
+const StyledButton = styled(Button)`
+  align-items: center;
+  border: none;
+  border-radius: 8px;
+  box-shadow: rgb(14 14 44 / 40%) 0px -1px 0px 0px inset;
+  cursor: pointer;
+  display: inline-flex;
+  font-size: 14px;
+  font-weight: 600;
+  height: 40px;
+  justify-content: center;
+  letter-spacing: 0.03em;
+  line-height: 1;
+  opacity: 1;
+  outline: 0px;
+  padding: 0px 16px;
+  transition: background-color 0.2s ease 0s, opacity 0.2s ease 0s;
+`;
+
 const UserBlock: React.FC<Props> = ({ account, login, logout }) => {
   const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account);
   const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
   return (
     <div>
       {account ? (
-        <Button
+        <StyledButton
           scale="sm"
           variant="tertiary"
           onClick={() => {
@@ -23,16 +43,16 @@ const UserBlock: React.FC<Props> = ({ account, login, logout }) => {
           }}
         >
           {accountEllipsis}
-        </Button>
+        </StyledButton>
       ) : (
-        <Button
+        <StyledButton
           scale="sm"
           onClick={() => {
             onPresentConnectModal();
           }}
         >
-          Connect
-        </Button>
+          Connect wallet
+        </StyledButton>
       )}
     </div>
   );
