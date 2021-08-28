92920920ba8c2d8a8bb68d13dac5d1933d92d311
bitfriend <bitfriend@hotmail.com>
2021-08-28 22:30:37 +0200

    Move app menu from library to frontend

--- dist/widgets/Menu/index.d.ts
+++ dist/widgets/Menu/index.d.ts
@@ -1,5 +1,6 @@
 export { default as Menu } from "./Menu";
 export { default as AppMenu } from "./AppMenu";
-export { default as Avatar } from "./components/Avatar";
+export { default as MenuAvatar } from "./components/Avatar";
+export * as MenuIcons from "./icons";
 export { status as menuStatus, links as menuConfig } from "./config";
-export type { Language, LinkStatus, MenuEntry, NavProps, PushedProps } from "./types";
+export type { Language, LinkStatus, MenuEntry, NavProps, PanelProps, PushedProps } from "./types";
